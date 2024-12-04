add422590e36f00b1fdc6eb10f6e5dfa5c24b14a
Dinann <notxurd46@gmail.com>
2024-12-04 23:58:35 +0700

    style(transaction): make only transaction list scrollable

--- lib/admin/transactions/transactions.dart
+++ lib/admin/transactions/transactions.dart
@@ -29,182 +29,183 @@ class _TransactionsPageState extends State<TransactionsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xFF232429),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Transaction List',
-                  style: TextStyle(
+      backgroundColor: const Color(0xFF232429),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Transaction List',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 24),
+              ),
+              Text(
+                'Dibawah merupakan daftar Transaksi yang ada di toko GameHeaven.',
+                style: TextStyle(
+                    color: Colors.grey[400], fontWeight: FontWeight.w100),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                          context, InputTransactionPage.routeName);
+                    },
+                    icon: const Icon(
+                      Icons.add,
                       color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 24),
-                ),
-                Text(
-                  'Dibawah merupakan daftar Transaksi yang ada di toko GameHeaven.',
-                  style: TextStyle(
-                      color: Colors.grey[400], fontWeight: FontWeight.w100),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        Navigator.pushNamed(
-                            context, InputTransactionPage.routeName);
-                      },
-                      icon: const Icon(
-                        Icons.add,
-                        color: Colors.white,
+                    ),
+                    label: const Text(
+                      'Add Transaction',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF121212),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 20),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
                       ),
-                      label: const Text(
-                        'Add Transaction',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF121212),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 12, vertical: 20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                SizedBox(
-                  height: 750,
-                  child: isLoading
-                      ? const Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : ListView.builder(
-                          itemCount: transactionsData.length,
-                          itemBuilder: (context, index) {
-                            final transaction = transactionsData[index];
-                            String formattedDate =
-                                formatDate(transaction['date']);
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              // Wrap the ListView in a Container with a fixed height
+              Container(
+                height: 600, // Set a fixed height for the ListView
+                child: isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView.builder(
+                        itemCount: transactionsData.length,
+                        itemBuilder: (context, index) {
+                          final transaction = transactionsData[index];
+                          String formattedDate =
+                              formatDate(transaction['date']);
 
-                            String formattedPrice =
-                                currencyFormatter(transaction["total_price"]);
+                          String formattedPrice =
+                              currencyFormatter(transaction["total_price"]);
 
-                            return InkWell(
-                              onTap: () {
-                                Navigator.pushNamed(
-                                    context, UpdateTransactionPage.routeName,
-                                    arguments: transaction);
-                              },
-                              child: Card(
-                                color: Colors.grey[850],
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 20, horizontal: 12),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          SizedBox(
-                                            width: 200,
-                                            child: Text(
-                                              "${transaction['products']['name']}",
-                                              style: const TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.w500),
-                                              overflow: TextOverflow.ellipsis,
-                                              maxLines: 1,
-                                            ),
-                                          ),
-                                          const SizedBox(
-                                            height: 4,
-                                          ),
-                                          Text(
-                                            "${transaction['username']}",
-                                            style: const TextStyle(
-                                                color: Colors.white),
-                                          ),
-                                          Row(
-                                            children: [
-                                              Text(
-                                                "${transaction['quantity']} Item",
-                                                style: const TextStyle(
-                                                    color: Colors.white),
-                                              ),
-                                              const SizedBox(
-                                                width: 12,
-                                              ),
-                                              Chip(
-                                                padding:
-                                                    const EdgeInsets.symmetric(
-                                                  vertical: 2,
-                                                ),
-                                                label: Text(
-                                                  "${transaction['status']}",
-                                                  style: const TextStyle(
-                                                      color: Colors.white,
-                                                      fontWeight:
-                                                          FontWeight.w400),
-                                                ),
-                                                backgroundColor: getStatusColor(
-                                                    transaction['status']),
-                                                shape: RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          20.0),
-                                                  side: BorderSide.none,
-                                                ),
-                                                elevation: 0,
-                                              )
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.end,
-                                        children: [
-                                          Text(
-                                            formattedPrice,
+                          return InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                  context, UpdateTransactionPage.routeName,
+                                  arguments: transaction);
+                            },
+                            child: Card(
+                              color: Colors.grey[850],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 20, horizontal: 12),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          width: 200,
+                                          child: Text(
+                                            "${transaction['products']['name']}",
                                             style: const TextStyle(
                                                 color: Colors.white,
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 18),
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w500),
+                                            overflow: TextOverflow.ellipsis,
+                                            maxLines: 1,
                                           ),
-                                          Text(
-                                            formattedDate,
-                                            style: const TextStyle(
-                                                color: Colors.white),
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
+                                        ),
+                                        const SizedBox(
+                                          height: 4,
+                                        ),
+                                        Text(
+                                          "${transaction['username']}",
+                                          style: const TextStyle(
+                                              color: Colors.white),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "${transaction['quantity']} Item${transaction['quantity'] > 1 ? 's' : ''}",
+                                              style: const TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                            const SizedBox(
+                                              width: 12,
+                                            ),
+                                            Chip(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                vertical: 2,
+                                              ),
+                                              label: Text(
+                                                "${transaction['status']}",
+                                                style: const TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight:
+                                                        FontWeight.w400),
+                                              ),
+                                              backgroundColor: getStatusColor(
+                                                  transaction['status']),
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20.0),
+                                                side: BorderSide.none,
+                                              ),
+                                              elevation: 0,
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: [
+                                        Text(
+                                          formattedPrice,
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ),
+                                        Text(
+                                          formattedDate,
+                                          style: const TextStyle(
+                                              color: Colors.white),
+                                        ),
+                                      ],
+                                    )
+                                  ],
                                 ),
                               ),
-                            );
-                          },
-                        ),
-                )
-              ],
-            ),
+                            ),
+                          );
+                        },
+                      ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   void getTransactionsData() async {
