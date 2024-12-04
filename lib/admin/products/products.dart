249747299a131ebea14726376362a755423d9859
Dinann <notxurd46@gmail.com>
2024-12-04 23:45:55 +0700

    feat(product): add product page

--- lib/admin/products/products.dart
+++ lib/admin/products/products.dart
@@ -1,5 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:toastification/toastification.dart';
 import 'package:uas_pemrograman_4_22411002_andreedyson/admin/products/input_product.dart';
+import 'package:uas_pemrograman_4_22411002_andreedyson/service/api.dart';
+import 'package:uas_pemrograman_4_22411002_andreedyson/utils/helpers.dart';
 
 class ProductsPage extends StatefulWidget {
   const ProductsPage({super.key});
@@ -11,6 +15,45 @@ class ProductsPage extends StatefulWidget {
 }
 
 class _ProductsPageState extends State<ProductsPage> {
+  final dio = Dio();
+  bool isLoading = false;
+  var dataProducts = [];
+
+  void getProductsData() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    await Future.delayed(const Duration(seconds: 1));
+    try {
+      Response response;
+
+      response = await dio.get(getProducts);
+
+      if (response.data["status"]) {
+        dataProducts = response.data["results"];
+      } else {
+        dataProducts = [];
+      }
+    } catch (e) {
+      toastification.show(
+          title: const Text("Terjadi kesalahan pada server"),
+          autoCloseDuration: const Duration(seconds: 3),
+          type: ToastificationType.error,
+          style: ToastificationStyle.fillColored);
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getProductsData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,6 +107,99 @@ class _ProductsPageState extends State<ProductsPage> {
             const SizedBox(
               height: 24,
             ),
+            isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: dataProducts.length,
+                      itemBuilder: (context, index) {
+                        final product = dataProducts[index];
+
+                        return Card(
+                            color: const Color(0xFF2f2f2f),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      // Product Image
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(8),
+                                        child: Image.network(
+                                          "$imageUrl/${product["image"]}",
+                                          width: 80,
+                                          height: 80,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 12,
+                                      ),
+                                      // Product Details
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          SizedBox(
+                                            width: 120,
+                                            child: Text(
+                                              product["name"],
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 16,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                              maxLines: 1,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 120,
+                                            child: Text(
+                                              "${product["categories"]["name"]} • ${product["brands"]["name"]}",
+                                              style: TextStyle(
+                                                  color: Colors.grey[400],
+                                                  fontSize: 14,
+                                                  height: 2,
+                                                  overflow:
+                                                      TextOverflow.ellipsis),
+                                              maxLines: 1,
+                                            ),
+                                          ),
+                                          Text(
+                                              product["stocks"] > 0
+                                                  ? "${product["stocks"]} Item${product["stocks"] > 1 ? 's' : ''}"
+                                                  : "Out of Stocks",
+                                              style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 14)),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Text(
+                                    currencyFormatter(
+                                      product["price"],
+                                    ),
+                                    style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14),
+                                  )
+                                ],
+                              ),
+                            ));
+                      },
+                    ),
+                  ),
           ],
         ),
       ),
