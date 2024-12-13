aded730f5444ba6923ca566be0cbe1214abd3645
Dinann <notxurd46@gmail.com>
2024-12-13 20:56:15 +0700

    revert(commits): go back to commit bac16de - fix admin login

--- lib/admin/transactions/input_transaction.dart
+++ lib/admin/transactions/input_transaction.dart
@@ -369,6 +369,33 @@ class _InputTransactionPageState extends State<InputTransactionPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
+                Container(
+                  width: 100,
+                  height: 46,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.red, width: 2),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      minimumSize: const Size.fromHeight(50),
+                      shadowColor: Colors.transparent,
+                    ),
+                    child: const Text(
+                      'Batal',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
                 SizedBox(
                   height: 46,
                   width: 100,
